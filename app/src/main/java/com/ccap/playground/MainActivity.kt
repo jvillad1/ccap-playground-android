87f1abeafec432021944e23d792a2c486a75a2da
Larissa Marães <larissa.maraes@mercadolivre.com>
2023-12-20 17:44:51 -0300

    Playing with modifiers

--- app/src/main/java/com/ccap/playground/MainActivity.kt
+++ app/src/main/java/com/ccap/playground/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.ccap.playground
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ccap.playground.ui.theme.CCapPlaygroundAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,15 +39,25 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val context = LocalContext.current
     CCapPlaygroundAndroidTheme {
-        Greeting("Android")
+        Greeting(
+            "Androidismo",
+            Modifier
+                .background(Color.Red)
+                .clickable {
+                    Toast
+                        .makeText(context, "Click!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+        )
     }
 }
