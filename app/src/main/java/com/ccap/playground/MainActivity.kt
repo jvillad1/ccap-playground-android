8a4289002f70d8ce0c49b670b03622a1a9730cc0
Larissa Marães <larissa.maraes@mercadolivre.com>
2024-01-24 17:44:54 -0300

    Adding button + State

--- app/src/main/java/com/ccap/playground/MainActivity.kt
+++ app/src/main/java/com/ccap/playground/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ccap.playground.ui.theme.CCapPlaygroundAndroidTheme
@@ -33,13 +37,24 @@ private fun MyApp(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("Lari", "Rodo")
 ) {
+    val expanded = remember { mutableStateOf(false) }
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
         Column {
             for (name in names) {
-                Greeting(name = name)
+                Row(modifier = Modifier.padding(24.dp)) {
+                    Column(modifier = Modifier.weight(1f)) {
+                        Text(text = "Hello ")
+                        Text(text = name)
+                    }
+                    ElevatedButton(
+                        onClick = { expanded.value = !expanded.value },
+                    ) {
+                        Text(if (expanded.value) "Show less" else "Show more")
+                    }
+                }
             }
         }
     }
