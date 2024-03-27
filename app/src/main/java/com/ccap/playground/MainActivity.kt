e0cbb50a488f77cb87f6981d66627bab0b6f1a81
MatheusMatz <maestrom217@gmail.com>
2024-03-27 16:20:03 -0300

    [Step 12]Styling and theming your app

--- app/src/main/java/com/ccap/playground/MainActivity.kt
+++ app/src/main/java/com/ccap/playground/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ccap.playground
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -22,11 +23,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ccap.playground.ui.theme.CCapPlaygroundAndroidTheme
@@ -81,6 +83,16 @@ fun OnboardingScreen(
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun OnboardingPreviewDarkMode() {
+    CCapPlaygroundAndroidTheme {
+        OnboardingScreen {
+            Log.i("Test", "Test Preview")
+        }
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
@@ -122,10 +134,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Column(
                 modifier = modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold,
+                        fontStyle = FontStyle.Italic
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded },
